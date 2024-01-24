8eb3760ac3e3d32ad08c5ab771b604c8ecd47c4d
Nikolay.Rusev <nikolay.rusev.main@gmail.com>
2024-01-24 16:47:31 +0200

    Commit

--- src/test/java/Utils/browserFactory.java
+++ src/test/java/Utils/browserFactory.java
@@ -1,4 +1,25 @@
 package Utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
 public class browserFactory {
+    WebDriver driver;
+
+    public void setup() {
+        ChromeOptions opt = new ChromeOptions();
+        opt.addExtensions(new File("C:\\extension\\uBlock.crx"));
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(opt);
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://automationexercise.com/");
+
+    }
 }
